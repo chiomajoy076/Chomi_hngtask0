843d34a7823ebe2bf28cf0697e31b5b6dade65b1
Chioma Joy <chiomajoy076@gmail.com>
2025-10-19 14:23:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function updateTime() {
+  const timeElement = document.getElementById("current-time");
+  if (timeElement) {
+    timeElement.textContent = Date.now();
+  }
+}
+updateTime();
+setInterval(updateTime, 1000);
